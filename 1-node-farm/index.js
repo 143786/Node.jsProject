ca798760f32a3f99bef0c797c85d8a8cac4b5552
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-12-20 12:00:59 +0100

    Add files via upload

---
+++ 1-node-farm/index.js
@@ -0,0 +1,8 @@
+const fs = require("fs"); /* access for reading data,
+writing data to the file system. (fs = file system)
+ calling the require() function here with this 
+ built-in fs module name will then return an object
+  in which there are lots of functions that can use */
+
+const hello = "Hello world";
+console.log(hello);
